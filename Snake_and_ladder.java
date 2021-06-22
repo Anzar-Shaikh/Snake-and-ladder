fc1b2c8f6baf10503bfb8f89a25d17aa4999f58e
anzar shaikh <anzarshaikh373@gmail.com>
2021-06-22 15:12:23 +0530

    Dieroll for 1-6 completerd

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -1,17 +1,12 @@
 package com.bl.basic;
-
 import java.util.Random;
 
 class gamePlayer {
     int currentPosition = 0;
     Random rand = new Random();
-    int attempts = 0;
-    final int noPlay = 0, ladder = 1, snake = 2;
-    int endGame = 0;
-
+    int dieRoll = rand.nextInt(6)+1;
 }
 public class Snake_and_ladder {
-
     public static void main(String[] args) {
         System.out.println("welcome to snake & ladder game");
     }
