52fc9f5e1627895e5ce6b1962bec32b48d54afcb
anzar shaikh <anzarshaikh373@gmail.com>
2021-06-22 14:56:52 +0530

    Game for single player is initialised.

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -1,7 +1,15 @@
 package com.bl.basic;
 
+import java.util.Random;
 
+class gamePlayer {
+    int currentPosition = 0;
+    Random rand = new Random();
+    int attempts = 0;
+    final int noPlay = 0, ladder = 1, snake = 2;
+    int endGame = 0;
 
+}
 public class Snake_and_ladder {
 
     public static void main(String[] args) {
