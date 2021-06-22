386dd6b7fa629d2c967486a6589a3ff452281694
anzar shaikh <anzarshaikh373@gmail.com>
2021-06-22 15:21:08 +0530

    start from zero condition ensurad.

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -5,11 +5,46 @@ class gamePlayer {
     Random rand = new Random();
     int currentPosition = 0;
     final int noPlay = 0, ladder = 1, snake = 2;
-    int dieRoll = rand.nextInt(6)+1;
-    int gameCheck = rand.nextInt(3);
+    int endGame = 0;
+    int attempts = 0;
+    public void gameStart(String playerName) {
+        while (endGame <= 100) {
+            int rollDie = rand.nextInt(6) + 1;
+            System.out.println("rollDie is :" + rollDie);
+            int gameCheck = rand.nextInt(3);
+            System.out.println("game Check is " + gameCheck);
+
+            if (gameCheck == noPlay) {
+                currentPosition = currentPosition;
+                System.out.println("This is no play.");
+                System.out.println(playerName + "'s current position is : " + currentPosition);
+                attempts++;
+            } else if (gameCheck == ladder) {
+                currentPosition = currentPosition + rollDie;
+                System.out.println("You got promoted by " + rollDie);
+                System.out.println(playerName + "'s current position is : " + currentPosition);
+                attempts++;
+            } else {
+                if (currentPosition < rollDie) {
+                    currentPosition = 0;
+                    System.out.println("You got snake of " + rollDie + " star again from zero.");
+                    System.out.println(playerName + "'s current position is : " + currentPosition);
+                } else {
+                    currentPosition = currentPosition - rollDie;
+                    System.out.println("you got snake of " + rollDie);
+                    System.out.println(playerName + "'s current position is : " + currentPosition);
+                }
+                attempts++;
+            }
+            endGame = currentPosition;
+        }
+
+    }
 }
+
 public class Snake_and_ladder {
     public static void main(String[] args) {
         System.out.println("welcome to snake & ladder game");
+        
     }
 }
