492d860ccaf9f6d28a5c4ab1a5b58e9c32a32144
anzar shaikh <anzarshaikh373@gmail.com>
2021-06-22 15:16:16 +0530

    no play , ladder & snake is added in game.

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -2,9 +2,11 @@ package com.bl.basic;
 import java.util.Random;
 
 class gamePlayer {
-    int currentPosition = 0;
     Random rand = new Random();
+    int currentPosition = 0;
+    final int noPlay = 0, ladder = 1, snake = 2;
     int dieRoll = rand.nextInt(6)+1;
+    int gameCheck = rand.nextInt(3);
 }
 public class Snake_and_ladder {
     public static void main(String[] args) {
