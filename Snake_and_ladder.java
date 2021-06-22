4ef510687e05929cd2bd8f2f948848c573c26f7d
anzar shaikh <anzarshaikh373@gmail.com>
2021-06-22 14:51:39 +0530

    welcome to snake & ladder game

---
+++ Snake_and_ladder.java
@@ -0,0 +1,10 @@
+package com.bl.basic;
+
+
+
+public class Snake_and_ladder {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to snake & ladder game");
+    }
+}
